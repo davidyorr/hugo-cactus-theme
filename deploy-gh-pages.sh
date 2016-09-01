0679ca56d63a30fe1ed2fe1d2e610b51c3ec3d40
David Orr <davidyorr@gmail.com>
2016-09-01 13:48:22 -0400

    Add a script for deploying gh-pages

---
+++ deploy-gh-pages.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# generate the site
+./hugoThemeSiteScript/generateThemeSite.sh
+hugo -s hugoThemeSite/themeSite
+
+# clone master into a temp directory
+TMP_DIR=temp
+rm -rf $TMP_DIR
+git clone git@github.com:davidyorr/hugo-orchid-theme.git $TMP_DIR
+cd $TMP_DIR
+
+# checkout the gh-pages branch and copy the site into it
+git checkout -t origin/gh-pages
+rm -rf *
+cp -R ../hugoThemeSite/themeSite/public/* .
+
+# push changes
+git add -A
+git commit -m "Built $(date)"
+git push origin gh-pages
+
+# delete the temp directory
+cd ..
+rm -rf $TMP_DIR
